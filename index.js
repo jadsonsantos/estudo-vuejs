0d7f06deae314f6f1d46cf60a38bbb88dd377e17
Jadson Santos <jadsondossantos5@gmail.com>
2020-04-05 15:02:05 -0300

    Adiciona diretiva v-for

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ var app = new Vue({
     el: '#app',
     data: {
         message: 'Hello Vue!',
-        seen: false
+        seen: false,
+        todos: [
+            { text: 'Learn JavaScript' },
+            { text: 'Learn Vue' },
+            { text: 'Build something awesome' },
+        ]
     }
 });
 
 app.seen = true;
+app.todos.push({ text: 'New item' })
