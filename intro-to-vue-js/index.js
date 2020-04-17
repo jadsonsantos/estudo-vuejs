2798f0a600b3aeafd091f508d3ceb9c7341ca461
Jadson Santos <jadsondossantos5@gmail.com>
2020-04-16 22:57:05 -0300

    List Rendering

--- intro-to-vue-js/index.js
+++ intro-to-vue-js/index.js
@@ -3,8 +3,10 @@ var app = new Vue({
     data: {
         product: 'Socks',
         image: './assets/vmSocks-green.jpg',
+        onSale: true,
         inStock: true,
         inventory: 5,
+        details: ['80% cotton', '20% poliester', 'Gender-neutral'],
         altText: 'A pair of socks',
         link: 'https://www.vuemastery.com/'
     }
