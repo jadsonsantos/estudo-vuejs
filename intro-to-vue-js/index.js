904742d68d6e7992b982a20f83a14e9613e2e514
Jadson Santos <jadsondossantos5@gmail.com>
2020-04-16 23:07:17 -0300

    Iterating Over Objects

--- intro-to-vue-js/index.js
+++ intro-to-vue-js/index.js
@@ -7,6 +7,17 @@ var app = new Vue({
         inStock: true,
         inventory: 5,
         details: ['80% cotton', '20% poliester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
         altText: 'A pair of socks',
         link: 'https://www.vuemastery.com/'
     }
