1a6b6f63cb71b3a16b44ddcff83e81ee05e5f9d3
Jadson Santos <jadsondossantos5@gmail.com>
2020-04-16 22:15:44 -0300

    Conditional Rendering

---
+++ intro-to-vue-js/conditional-rendering/index.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        image: './assets/vmSocks-green.jpg',
+        inStock: true,
+        altText: 'A pair of socks',
+        link: 'https://www.vuemastery.com/'
+    }
+});
