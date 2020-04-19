9b96d1f307d3a902f487c444f8882f9e4192aedc
Jadson Santos <jadsondossantos5@gmail.com>
2020-04-18 22:40:49 -0300

    Refatora código como componente

--- intro-to-vue-js/index.js
+++ intro-to-vue-js/index.js
@@ -1,30 +1,80 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-        product: 'Socks',
-        brand: 'Vue Mastery',
-        selectedVariant: 0,
-        onSale: true,
-        inventory: 0,
-        details: ['80% cotton', '20% poliester', 'Gender-neutral'],
-        variants: [
-            {
-                variantId: 2234,
-                variantColor: 'green',
-                variantImage: 'assets/vmSocks-green.jpg',
-                variantQuantity: 0
-            },
-            {
-                variantId: 2235,
-                variantColor: 'blue',
-                variantImage: 'assets/vmSocks-blue.png',
-                variantQuantity: 10
-            }
-        ],
-        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
-        cart: 0,
-        altText: 'A pair of socks',
-        link: 'https://www.vuemastery.com/'
+Vue.component('product', {
+    template: `
+        <div class="product">
+            <div class="product-image">
+                <img v-bind:src="image" v-bind:alt="altText" />
+            </div>
+            <div class="product-info">
+                <div class="cart">
+                    <p>Cart {{ cart }}</p>
+                </div>
+                <h1>{{ title }}</h1>
+                <p v-if="inStock">In Stock</p>
+                <p v-else
+                    :class="{ outOfStock: !inStock }">Out of Stock
+                </p>
+                <p>{{ sale }}</p>
+                <a v-bind:href="link">{{ product }}</a>
+                <ul>
+                    <li v-for="detail in details">{{ detail }}</li>
+                </ul>
+                <ul>
+                    <li v-for="size in sizes">{{ size }}</li>
+                </ul>
+                <div class="color-box" 
+                        v-for="(variant, index) in variants" 
+                        :key="variant.variantId"
+                        :style="{ backgroundColor: variant.variantColor }"
+                        @mouseover="updateProduct(index)"
+                        >
+                </div>
+                <button @click="removeFromCart">Remove Cart</button>
+                <button v-on:click="addToCart"
+                        :disabled="!inStock"
+                        :class="{ disabledButton: !inStock }"
+                        >Add to Cart
+                </button>
+            </div>
+        </div>
+    `,
+    data() {
+        return {
+            product: 'Socks',
+            brand: 'Vue Mastery',
+            selectedVariant: 0,
+            onSale: true,
+            inventory: 0,
+            details: ['80% cotton', '20% poliester', 'Gender-neutral'],
+            variants: [
+                {
+                    variantId: 2234,
+                    variantColor: 'green',
+                    variantImage: 'assets/vmSocks-green.jpg',
+                    variantQuantity: 0
+                },
+                {
+                    variantId: 2235,
+                    variantColor: 'blue',
+                    variantImage: 'assets/vmSocks-blue.png',
+                    variantQuantity: 10
+                }
+            ],
+            sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+            cart: 0,
+            altText: 'A pair of socks',
+            link: 'https://www.vuemastery.com/'
+        }
+    },
+    methods: {
+        addToCart: function() {
+            this.cart += 1
+        },
+        removeFromCart: function() {
+            this.cart -= 1
+        },
+        updateProduct: function(index) {
+            this.selectedVariant = index
+        }
     },
     computed: {
         title() {
@@ -42,16 +92,9 @@ var app = new Vue({
             }
                 return this.brand + ' ' + this.product + ' are not on sale'
         }
-    },
-    methods: {
-        addToCart() {
-            this.cart += 1
-        },
-        removeFromCart() {
-            this.cart -= 1
-        },
-        updateProduct(index) {
-            this.selectedVariant = index
-        }
-    },
+    }
+})
+
+var app = new Vue({
+    el: '#app'
 });
